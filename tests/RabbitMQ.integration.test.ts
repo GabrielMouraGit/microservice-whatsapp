2367280b3aa0aeb6129771d8789b7f6fbad10f51
GabrielMouragit <email.gabriel.moura@gmail.com>
2026-04-27 23:04:53 -0300

    feat: add  test rabbitMQ

---
+++ tests/RabbitMQ.integration.test.ts
@@ -0,0 +1,26 @@
+import { describe, expect, it } from "vitest";
+import { RabbitMQPublisher } from "../src/infrastructure/messaging/rabbit/RabbitMQPublisher";
+import { RabbitMQConsumer } from "../src/infrastructure/messaging/rabbit/RabbitMQConsumer";
+
+describe("RabbitMQ integration", () => {
+  it("should publish and consume message", async () => {
+    const publisher = new RabbitMQPublisher();
+    const consumer = new RabbitMQConsumer();
+
+    const received: any[] = [];
+
+    await consumer.consume("messages.upsert", async (data) => {
+      received.push(data);
+    });
+
+    await publisher.publish("messages.upsert", {
+      hello: "world",
+    });
+
+    // espera processamento async
+    await new Promise((r) => setTimeout(r, 500));
+
+    expect(received.length).toBe(1);
+    expect(received[0]).toEqual({ hello: "world" });
+  });
+});
