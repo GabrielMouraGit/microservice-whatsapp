97650ad6b8f6dedff4148c24b132a7e27cf7a4f5
GabrielMouragit <email.gabriel.moura@gmail.com>
2026-05-06 08:40:23 -0300

    feat: add Handler Auth plugin

---
+++ src/interfaces/plugins/HandlerAuth.ts
@@ -0,0 +1,47 @@
+import fp from "fastify-plugin";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { $config } from "@config/config";
+
+declare module "fastify" {
+  interface FastifyRequest {
+    auth: {
+      tenant_id: string;
+      user_id: string;
+    };
+  }
+}
+
+export default fp(async function (fastify) {
+  fastify.decorateRequest("auth");
+
+  fastify.addHook(
+    "preHandler",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      if (request.url.startsWith("/public")) {
+        return;
+      }
+
+      const gatewaySecret = request.headers["x-gateway-secret"] as string;
+
+      if (gatewaySecret !== $config.GATEWAY_SECRET) {
+        return reply.status(401).send({
+          message: "Unauthorized gateway",
+        });
+      }
+
+      const tenant_id = request.headers["x-tenant-id"] as string;
+      const user_id = request.headers["x-user-id"] as string;
+
+      if (!tenant_id) {
+        return reply.status(401).send({
+          message: "tenant_id missing",
+        });
+      }
+
+      request.auth = {
+        tenant_id,
+        user_id,
+      };
+    },
+  );
+});
