e05885757c6e83018461867b79fb66d95518d4c9
GabrielMouragit <email.gabriel.moura@gmail.com>
2026-05-15 13:28:35 -0300

    fix: RabbitMQConnection

--- src/infrastructure/messaging/rabbit/RabbitMQConnection.ts
+++ src/infrastructure/messaging/rabbit/RabbitMQConnection.ts
@@ -1,11 +1,12 @@
-import { $config } from "@config/config";
-import amqp, { Connection, Channel } from "amqplib";
+import amqp, { Channel, Connection } from "amqplib";
 
 export class RabbitMQConnection {
   private static instance: RabbitMQConnection;
 
   private connection!: Connection;
   private channel!: Channel;
+
+  private isConnecting = false;
 
   private constructor() {}
 
@@ -18,9 +19,59 @@ export class RabbitMQConnection {
     return this.instance;
   }
 
-  private async connect() {
-    this.connection = await amqp.connect($config.RABBITMQ_URL);
+  async connect() {
+    if (this.isConnecting) return;
 
+    this.isConnecting = true;
+
+    try {
+      console.log("conectando no rabbitmq...");
+
+      this.connection = await amqp.connect(process.env.RABBITMQ_URL!);
+
+      this.connection.on("error", (err) => {
+        console.error("❌ rabbit connection error:", err);
+      });
+
+      this.connection.on("close", async () => {
+        console.error("⚠️ rabbit connection closed");
+
+        this.isConnecting = false;
+
+        setTimeout(async () => {
+          try {
+            await this.connect();
+          } catch (err) {
+            console.error("❌ erro reconnect:", err);
+          }
+        }, 5000);
+      });
+
+      this.channel = await this.connection.createChannel();
+
+      this.channel.on("error", (err) => {
+        console.error("❌ channel error:", err);
+      });
+
+      this.channel.on("close", () => {
+        console.error("⚠️ channel closed");
+      });
+
+      console.log("✅ rabbit conectado");
+
+      this.isConnecting = false;
+    } catch (err) {
+      console.error("❌ falha rabbit:", err);
+
+      this.isConnecting = false;
+
+      setTimeout(async () => {
+        await this.connect();
+      }, 5000);
+    }
+  }
+
+  async recreateChannel() {
     this.channel = await this.connection.createChannel();
   }
 
